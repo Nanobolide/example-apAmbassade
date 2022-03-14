4273d3d7e4211b05ce81d977da3bc1fa6ab94140
225junior <225juniorbrindou@gmail.com>
2022-03-14 09:07:20 +0000

    Update login form: 1- delete bootstrap 4 cdn to bootstrap 5 present in vendor folder 2- Update symbole of rca for login page 3- update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,67 @@
 @extends('layouts.authApp')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <h2 class="text-center text-dark">Ambassade RCA</h2>
+                <div class="text-center mb-1 text-dark">Connexion</div>
+                <div class="card my-1">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" action="{{ route('login') }}" class="card-body shadow-lg cardbody-color p-lg-3">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="text-center">
+                            <img src="{{ asset('rca/symbole.png')}}"
+                                class="img-fluid profile-image-pic img-thumbnail mb-3" width="200px"
+                                alt="profile">
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="mb-3">
+                            <input type="text" class="form-control  @error('email') is-invalid @enderror" id="email"
+                                aria-describedby="email" placeholder="Addresse Email" name="email"
+                                value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
+
+
+
+                        <div class="mb-3">
+                            <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                name="password" required autocomplete="current-password" id="password"
+                                placeholder="password">
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary px-5 mb-4 w-100">Connexion</button>
                         </div>
+
+                        <div id="emailHelp" class="form-text text-center mb-1 text-dark">Pas de compter? <a
+                                class="text-dark fw-bold" href="{{ route('register') }}">Créer un compte</a>
+                        </div>
+
+                        <div id="emailHelp" class="form-text text-center mb-5 text-dark">Vous avez oublié votre mot de passe?
+                            @if (Route::has('password.request'))
+                            <a class="text-dark fw-bold"  href="{{ route('password.request') }}">Réinitialisez votre compte ici</a>
+                            @endif
+                        </div>
+
                     </form>
                 </div>
+
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/authApp.blade.php
+++ resources/views/layouts/authApp.blade.php
@@ -18,9 +18,10 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.css') }}" >
 </head>
 <body>
+    @auth
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
@@ -40,19 +41,7 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
@@ -70,13 +59,13 @@
                                     </form>
                                 </div>
                             </li>
-                        @endguest
                     </ul>
                 </div>
             </div>
         </nav>
+        @endauth
 
-        <main class="py-4">
+        <main>
             @yield('content')
         </main>
     </div>
